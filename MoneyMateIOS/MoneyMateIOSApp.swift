df8d38619dc1e3ebdb2b349ad6ad508ad8c8303b
Asawin <asawin.krongprasert@outlook.com>
2024-08-31 16:31:26 +0700

    Initial Commit

---
+++ MoneyMateIOS/MoneyMateIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMateIOSApp.swift
+//  MoneyMateIOS
+//
+//  Created by Asawin Krongprasert on 31/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMateIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
