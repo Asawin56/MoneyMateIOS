6df35f49edc8d5f929554b6ac5874ea164b92c21
Asawin Krongprasert <asawin@Asawins-MacBook-Air.local>
2025-05-03 02:20:49 +0700

    Add HomeView to project

---
+++ MoneyMateIOS/MoneyMateIOS/Views/HomeView.swift
@@ -0,0 +1,50 @@
+//
+//  HomeView.swift
+//  MoneyMateIOS
+//
+//  Created by Asawin Krongprasert on 3/5/2568 BE.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack{
+            TabView {
+                Text("Page 1")
+                
+                Text("Page 2")
+                
+                Text("Page 3")
+            }
+            .tabViewStyle(.page)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading){
+                    Button {
+                        print("Logout")
+                    } label: {
+                        Image(systemName:
+                                "person.circle")
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing){
+                    Button {
+                        print("Navigate to new item")
+                    } label: {
+                        Image(systemName:
+                                "plus")
+                    }
+                }
+            }
+            .navigationDestination(for:
+                                    String.self) { _ in
+                Text("New view hear")
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
